ba2398efd00f3171dd9641034bd80a83b25c2e01
deidmon <paul.dedios@alum.udep.edu.pe>
2024-11-28 09:41:37 -0500

    Modificar calculo de horas trabajadas

--- proyecto-sql/consult-data.sql
+++ proyecto-sql/consult-data.sql
@@ -5,6 +5,10 @@ INNER JOIN Marca M ON E.EmpleadoID = M.EmpleadoID
 INNER JOIN TipoMarca T ON M.Tipo = T.Tipo
 
 --Calculating hours worked
+DECLARE @FechaInicial DATE = '2024-11-25'
+DECLARE @FechaFinal DATE = GETDATE()
+
+
 SELECT EmpleadoID, CONVERT(DATE, Fecha),
        MAX(CASE WHEN Tipo = 'EI' THEN Fecha END),
        MAX(CASE WHEN Tipo = 'EF' THEN Fecha END),
@@ -18,10 +22,22 @@ SELECT EmpleadoID, Fecha, ISNULL(DATEDIFF(HOUR, EI, EF),0) + ISNULL(DATEDIFF(HOU
 INTO #HorasTrabajadas
 FROM #Marcas
 
-SELECT E.EmpleadoID, E.Nombre, E.Apellido, E.SueldoHora,
-       H.Fecha, H.Horas, (E.SueldoHora * H.Horas) AS TotalPagar
+SELECT E.EmpleadoID, E.Nombre, E.Apellido, E.SueldoHora AS SueldoHora,
+       SUM(H.Horas) AS Horas, (Horas * TotalPagar) AS TotalPagar
+INTO #HorasTotal
 FROM #HorasTrabajadas H 
 INNER JOIN Empleado E ON H.EmpleadoID = E.EmpleadoID
+WHERE H.Fecha BETWEEN @FechaInicial AND @FechaFinal
+GROUP BY E.EmpleadoID, E.Nombre, E.Apellido
 
+SELECT *
+FROM #HorasTotal
+
+ALTER TABLE #HorasTotal
+ADD TotalPagar MONEY
+
+UPDATE #HorasTotal SET TotalPagar = CASE WHEN Horas <=40 THEN Horas - 0.5 ELSE Horas END * SueldoHora
+
+DROP TABLE #HorasTotal
 DROP TABLE #HorasTrabajadas
 DROP TABLE #Marcas
